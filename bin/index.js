636f2ffb526cc0fccb8c0e40d46353eff3cef07c
StoneZol <miha2000rus@gmail.com>
2025-06-26 13:34:37 +0300

    first commit

---
+++ bin/index.js
@@ -0,0 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { convertImages } from "../lib/converter.js";
+import fs from "fs/promises";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const CONFIG_PATH = path.resolve(process.cwd(), "image-converter.config.js");
+
+try {
+  const { default: config } = await import(CONFIG_PATH);
+  await convertImages(config);
+} catch (e) {
+  console.error("❌ Ошибка загрузки конфигурации:", e.message);
+  process.exit(1);
+}
---
+++ image-converter.config.js
@@ -0,0 +1,7 @@
+export default {
+    dir: './public',     // что сканировать
+    format: 'webp',             // или 'avif'
+    quality: 80,
+    removeOriginal: true
+  }
+  
