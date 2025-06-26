b6f9de7b987231cc3d89543301731daeea143401
StoneZol <miha2000rus@gmail.com>
2025-06-26 14:10:46 +0300

    108

--- image-converter.config.mjs
+++ image-converter.config.mjs
@@ -1,7 +1,6 @@
 export default {
-    dir: './public',     // что сканировать
-    format: 'webp',             // или 'avif'
-    quality: 80,
-    removeOriginal: true
-  }
-  
+  dir: "./public",
+  format: "webp",
+  quality: 80,
+  removeOriginal: true,
+};
